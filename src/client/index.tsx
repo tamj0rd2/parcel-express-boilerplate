efce3dc219442d3230da7086eca776d4fcaa6cef
Tamara Jordan <tamara.jordan1+coding@hotmail.com>
2020-09-09 21:15:25 +0100

    Add react

---
+++ src/client/index.tsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react'
+import ReactDOM from 'react-dom'
+
+console.log('Hello from tsx!')
+
+const App = () => {
+  const [response, setResponse] = useState<string>()
+
+  useEffect(() => {
+    fetch('/api/healthcheck')
+      .then((res) => {
+        if (!res.ok) throw new Error(`Received bad status ${res.status}`)
+        return res.text()
+      })
+      .then((text) => setResponse(text))
+      .catch(() => setResponse('Something went wrong :('))
+  }, [])
+
+  return (
+    <main>
+      <h1>Hello :)</h1>
+      <h2>{response}</h2>
+    </main>
+  )
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root'),
+)
